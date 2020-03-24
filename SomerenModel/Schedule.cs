84d6c9962385b86f3d1a49a2f29b8bc326c67490
Davey Smit <davey.smit10@gmail.com>
2020-03-24 22:54:06 +0100

    add schedule class

---
+++ SomerenModel/Schedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Schedule
+    {
+        DateTime Date { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+    }
+}
